06f5a9a45b5c3f38d6fbfc232e7a181347d99b7f
welcome.suresh2012@gmail.com <welcome.suresh2012@gmail.com>
2020-07-29 06:53:49 +0530

    file upload add

--- TestProject/src/main/java/com/test/wesure/FileUploadGCP.java
+++ TestProject/src/main/java/com/test/wesure/FileUploadGCP.java
@@ -1,34 +1,90 @@
 package com.test.wesure;
 
-import java.io.FileInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import com.google.common.collect.Lists;
 
 public class FileUploadGCP {
-	
-	@RequestMapping(value = "gcp", method = RequestMethod.GET)
-	public void authExplicit() throws IOException {
-		String jsonPath = "./WEB-INF/itlpics-763936d5cc5a.json";
-		  // You can specify a credential file by providing a path to GoogleCredentials.
-		  // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
-		  GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
-		        .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-		  Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
 
-		  System.out.println("Buckets:");
-		  Page<Bucket> buckets = storage.list();
-		  for (Bucket bucket : buckets.iterateAll()) {
-		    System.out.println(bucket.toString());
-		  }
-		}
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
+	public ModelAndView fileUpload() {
+		System.out.println("----------");
+		return new ModelAndView("fileUpload");
+	}
 	
+	// School Logo Upload Start
+		@RequestMapping(value = "fileUpload", method = RequestMethod.POST)
+		public String saveDealerDetail(HttpServletRequest request, HttpServletResponse response, HttpSession session,
+				@RequestParam("logoUpload") MultipartFile uploadfile) throws IOException {
+
+			// Get the filename and build the local file path (be sure that the
+			// application have write permissions on such directory)
+			String originaleRilename = uploadfile.getOriginalFilename();
+		/*
+		 * String directory = context.getRealPath("/") + "schoolLogo";
+		 * System.out.println("Upload dir---<>"+directory);
+		 */
+			String fileExtention = getExtensionByStringHandling(originaleRilename).get();
+//			commonFileUpload(uploadfile, , originaleRilename + "");
+			
+			uploadObject("itlpics", "itlpics_bucket", "VKS", uploadfile.getName());
+			return "Success";
+
+		}
+		
+		// get file Extension start
+		public static Optional<String> getExtensionByStringHandling(String filename) {
+			return Optional.ofNullable(filename).filter(f -> f.contains("."))
+					.map(f -> f.substring(filename.lastIndexOf(".") + 1));
+		}
+		// get file Extension end
+		
+//		file Upload GCP bucket
+		public static void uploadObject(
+			      String projectId, String bucketName, String objectName, String filePath) throws IOException {
+			    // The ID of your GCP project
+			    // String projectId = "your-project-id";
+
+			    // The ID of your GCS bucket
+			    // String bucketName = "your-unique-bucket-name";
+
+			    // The ID of your GCS object
+			    // String objectName = "your-object-name";
+
+			    // The path to your file to upload
+			    // String filePath = "path/to/your/file"
+
+			    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+			    BlobId blobId = BlobId.of(bucketName, objectName);
+			    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+			    storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
+
+			    System.out.println(
+			        "File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
+			  }
+
+		
+		
 }
